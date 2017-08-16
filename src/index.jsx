9ee1183d31821ff562e46d5fee8b9afaeca964c2
Tycho Price <tychoprice@gmail.com>
2017-08-15 23:30:18 -0700

    updated components

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,49 @@
-import React from 'react';
+import _ from 'lodash';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyB6NTqak2o9YIyVkrP-GuFFG4bPCrkg5pE';
 
-const App = () => {
-	return (
-		<div>
-			Search <SearchBar />
-		</div>
-	);
-};
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			videos:[],
+			selectedVideo: null
+		};
+
+		this.videoSearch('steve wozniak');
+	}
+
+	videoSearch(term) {
+		YTSearch({key:API_KEY, term: term}, (videos) => {
+			this.setState({
+				videos: videos,
+				selectedVideo: videos[0]
+			});
+		});
+	}
+
+	render() {
+		const videoSearch = _.debounce((term) => {
+			this.videoSearch(term);
+		}, 300);
+
+		return (
+			<div>
+				<SearchBar onSearchTermChange={videoSearch} />
+				<VideoDetail video={this.state.selectedVideo}/>
+				<VideoList
+					onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+					videos={this.state.videos} />
+			</div>
+		);
+	}
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
